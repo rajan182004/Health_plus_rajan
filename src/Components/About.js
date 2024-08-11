3330efe34e6710bb53023bab098a914c0e33c07e
rajan182004 <rajandon747@gmail.com>
2024-08-11 23:13:09 +0530

    code push to github

---
+++ src/Components/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Doctor from "../Assets/doctor-group.png";
+import SolutionStep from "./SolutionStep";
+import "../Styles/About.css";
+
+function About() {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-image-content">
+        <img src={Doctor} alt="Doctor Group" className="about-image1" />
+      </div>
+
+      <div className="about-text-content">
+        <h3 className="about-title">
+          <span>About Us</span>
+        </h3>
+        <p className="about-description">
+        Welcome to Health Plus, where compassionate care meets advanced medical expertise. Our dedicated team of professionals is committed to providing personalized treatment in a state-of-the-art facility. We prioritize your well-being with a holistic approach to health, ensuring a safe and healing environment. Experience the highest standard of healthcare with us.
+        </p>
+
+        <h4 className="about-text-title">Your Solutions</h4>
+
+        <SolutionStep
+          title="Choose a Specialist"
+          description="Find your perfect specialist and book with ease at Health Plus. Expert doctors prioritize your health, offering tailored care."
+        />
+
+        <SolutionStep
+          title="Make a Schedule"
+          description="Choose the date and time that suits you best, and let our dedicated team of medical professionals ensure your well-being with personalized care."
+        />
+
+        <SolutionStep
+          title="Get Your Solutions"
+          description="Our experienced doctors and specialists are here to provide expert advice and personalized treatment plans, helping you achieve your best possible health."
+        />
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Scripts/reviews.js
@@ -0,0 +1,52 @@
+export const customerReviews = [
+    {
+      "name": "Bhavishya Thota",
+      "location": "Machlipatnam, India",
+      "message": "Health Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "Kamishetti Sharan",
+      "location": "Hyderabad, India",
+      "message": "I found the perfect specialist for my condition through Health Plus. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Sweta Mishra",
+      "location": "Dhanbad, Jharkhand",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Niti Naik",
+      "location": "Gujrat, India",
+      "message": "Health Plus is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+    {
+      "name": "Biplup Ruchal",
+      "location": "Torronto, Canada",
+      "message": "I used to dread dental visits, but Health Plus made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+    },
+    {
+      "name": "Pragati, Patel",
+      "location": "Patna, Bihar",
+      "message": "Finally, a healthcare platform that puts patients first! The heart disease service provided comprehensive care and regular follow-ups. Thank you, Health Plus!"
+    },
+    {
+      "name": "Anshu Rani",
+      "location": "Motihari, Bihar",
+      "message": "I've been using Health Plus for a variety of health concerns, and each time, I received exceptional care. It's my go-to for all medical needs!"
+    },
+    {
+      "name": "Sakshi Dhawan",
+      "location": "Agra, India",
+      "message": "I highly recommend Health Plus for online consultations. It's convenient, secure, and the doctors are top-notch. Great job, team!"
+    },
+    {
+      "name": "Sarika Meena",
+      "location": "Kota, India",
+      "message": "The convenience of accessing medical notes online was fantastic. Health Plus made managing my health records hassle-free! Best Healthcare services."
+    },
+    {
+      "name": "Saher Hanspal",
+      "location": "Patiala, India",
+      "message": "As a busy professional, Health Plus has been a lifesaver. Quick prescriptions and refills without compromising on quality care. Thank you!"
+    }
+];
